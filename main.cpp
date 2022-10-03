0050fff5e9c53365445ccb6516330d5d9555bbba
jashchoksi <choksijash00@gmail.com>
2022-10-03 09:17:03 -0500

    main.cpp setup

--- main.cpp
+++ main.cpp
@@ -4,6 +4,8 @@
 #include <iostream>
 #include <time.h>
 
+const int A_ASCII_VALUE = 65;
+
 int main() {
     srand(time(0));
 
@@ -17,13 +19,25 @@ int main() {
     std::cout << "Enter time to run the load balancer: ";
     std::cin >> run_time;
 
-    LoadBalancer* load_balancer = new LoadBalancer();
+    std::vector<WebServer> webservers(num_servers, NULL);
+    for (int i = 0; i < num_servers; i++) {
+        char server_name = char(A_ASCII_VALUE + i);
+        WebServer* webserver = new WebServer(server_name);
+        webservers.at(i) = *webserver;
+    }
+
+    LoadBalancer* load_balancer = new LoadBalancer(webservers);
 
     requests_to_generate = num_servers * 2;
 
     for (int i = 0; i < requests_to_generate; i++) {
         Request* generated_request = new Request();
         load_balancer->pushNewRequest(*generated_request);
+    }
+
+    while (!load_balancer->isQueueEmpty()) {
+        load_balancer->incrementTime();
+        Request popped_request = load_balancer->popNextRequest();
     }
 
     return 0;
