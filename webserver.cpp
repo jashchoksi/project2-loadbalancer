6084f47a0890ef0dda11c552483555e8aef1214b
jashchoksi <choksijash00@gmail.com>
2022-10-11 19:03:02 -0500

    Update webserver files

--- webserver.cpp
+++ webserver.cpp
@@ -3,13 +3,24 @@
 WebServer::WebServer(char server_name) {
     this->server_name = server_name;
     this->request_process_start_time = 0;
+    this->has_request = false;
 }
 
 char WebServer::getServerName() { return this->server_name; }
 
-bool WebServer::isRequestProcessed(int current_time) { return (current_time >= (request_process_start_time + request.process_time)); }
+bool WebServer::hasRequest() { return this->has_request; }
+
+bool WebServer::isRequestProcessed(int current_time) {
+    if (current_time >= (request_process_start_time + request.process_time)) {
+        this->has_request = false;
+        return true;
+    } else {
+        return false;
+    }
+}
 
 void WebServer::processRequest(Request request, int current_time) {
     this->request = request;
     this->request_process_start_time = current_time;
+    this->has_request = true;
 }
--- webserver.h
+++ webserver.h
@@ -7,9 +7,11 @@ class WebServer {
     public:
         WebServer(char server_name);
         char getServerName();
+        bool hasRequest();
         bool isRequestProcessed(int current_time);
         void processRequest(Request request, int current_time);
     private:
+        bool has_request;
         Request request;
         int request_process_start_time;
         char server_name;
